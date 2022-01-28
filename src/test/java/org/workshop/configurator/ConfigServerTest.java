fabd3b4d2f5122f6572a3f18506e39e671812c66
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2022-01-28 10:18:06 +0000

    simplify logic. remove kotlin

---
+++ src/test/java/org/workshop/configurator/ConfigServerTest.java
@@ -0,0 +1,14 @@
+package org.workshop.configurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
